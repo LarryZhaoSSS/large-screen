3d12d15626cc69fc50079ebb399c32f8c13d51f9
LarryKameZhao <larryzhao1994@outlook.com>
2021-04-24 20:51:51 +0800

    set pageWith and pageSize

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.scss';
-
+import {HashRouter,Route,Switch} from "react-router-dom";
+import React from 'react';
+import {Home} from "./pages/Home";
+import './shared/style.scss'
 function App() {
   return (
     <div className="App">
-
+      <HashRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home/>
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
